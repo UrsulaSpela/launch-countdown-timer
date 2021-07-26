0ef753ccdb84b3d1b31f5df47694031f64643897
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-07-26 14:30:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var days = document.querySelector('.days');
+var hours = document.querySelector('.hours');
+var minutes = document.querySelector('.minutes');
+var seconds = document.querySelector('.seconds');
+
+days.innerHTML ="08";
+hours.innerHTML="23";
+minutes.innerHTML="55";
+seconds.innerHTML = "41";
