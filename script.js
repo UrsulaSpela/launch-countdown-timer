8f64a072f0a0d221f8ae926196a1289ffcd0feee
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-09-29 13:21:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-var days = document.querySelector('.days');
-var hours = document.querySelector('.hours');
-var minutes = document.querySelector('.minutes');
-var seconds = document.querySelector('.seconds');
+const countdown = function(){
+    const endDate = new Date('January 1, 2022 00:00:00').getTime()
+    const now = new Date().getTime()
+    const timeGap = endDate - now
 
-days.innerHTML ="08";
-hours.innerHTML="23";
-minutes.innerHTML="55";
-seconds.innerHTML = "41";
+    // calculate time
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const dayText = Math.floor(timeGap / day)
+    const hourText = Math.floor((timeGap % day) / hour)
+    const minuteText = Math.floor((timeGap % hour) / minute)
+    const secondText = Math.floor((timeGap % minute) / second)
+
+    document.querySelector('.days').textContent = dayText
+    document.querySelector('.hours').textContent = hourText
+    document.querySelector('.minutes').textContent = minuteText
+    document.querySelector('.seconds').textContent = secondText
+}
+
+setInterval(countdown, 1000)
